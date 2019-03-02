7a2c21ff62917794967a350fe169daf5d1b67b54
ps1437 <praveen369soni@gmail.com>
2019-03-02 20:28:57 +0530

    Read me updated

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # springboot-jasypt-encryption
 Jasypt integration for Spring boot 
+
+
+Use the Encrypted password in application.properties
+key = ENC(ENCRYPTED_PASSWORD)
+
+
+
+---------------------------------------------------------------------------------------------
+Java configuration  - 1 Approach
+---------------------------------------------------------------------------------------------
+Application.properties - ADD BELOW LINE
+jasypt.encryptor.password=password
+
+//ADD BELOW BEAN IN MAIN CLASS
+@Bean(name = "encryptorBean")
+	public StringEncryptor stringEncryptor() {
+	    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+	    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+	    config.setAlgorithm("PBEWithMD5AndDES");
+	    config.setKeyObtentionIterations("1000");
+	    config.setPoolSize("1");
+	    config.setProviderName("SunJCE");
+	    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+	    config.setStringOutputType("base64");
+	    encryptor.setConfig(config);
+	    return encryptor;
+}
+
+
+
+---------------------------------------------------------------------------------------------
+Application.properties - 2 Approach
+---------------------------------------------------------------------------------------------
+ADD BELOW 3 PROPERTIES IN APPLICATION.properties file 
+jasypt.encryptor.bean=encryptorBean
+jasypt.encryptor.password=password
+jasypt.encryptor.algorithm=PBEWithMD5AndDES
+		
--- springboot-jasypt/src/main/java/com/syscho/jasypt/springbootjasypt/SpringbootJasyptApplication.java
+++ springboot-jasypt/src/main/java/com/syscho/jasypt/springbootjasypt/SpringbootJasyptApplication.java
@@ -4,11 +4,15 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 import com.syscho.jasypt.springbootjasypt.service.EmployeeService;
 import com.syscho.jasypt.springbootjasypt.vo.Employee;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
@@ -20,6 +24,25 @@ public class SpringbootJasyptApplication implements CommandLineRunner {
 	@Autowired
 	private StringEncryptor stringEncryptor;
 
+	/*
+	  //Java Configuration for jasypt Encryption
+	  //remove - jasypt.encryptor.bean=encryptorBean ,jasypt.encryptor.algorithm=PBEWithMD5AndDES values from application.properties)
+	  
+	@Bean(name = "encryptorBean")
+	public StringEncryptor stringEncryptor() {
+	    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+	    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+	    config.setAlgorithm("PBEWithMD5AndDES");
+	    config.setKeyObtentionIterations("1000");
+	    config.setPoolSize("1");
+	    config.setProviderName("SunJCE");
+	    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+	    config.setStringOutputType("base64");
+	    encryptor.setConfig(config);
+	    return encryptor;
+	}
+	*/
+	
 	@Autowired
 	private EmployeeService service;
 
@@ -33,13 +56,16 @@ public class SpringbootJasyptApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		
+		System.out.println("Decrepted pwd :"+ pwd);
 		System.out.println("Encrypted password  =>> " + stringEncryptor.encrypt("admin"));
+	
+        //Testing code	
 		System.out.println("ADD EMPLOYEE - " + service.addEmployee(new Employee("Jon")));
 		List<Employee> employees = service.getEmployees();
 
 		Iterator<Employee> iterator = employees.iterator();
 		while (iterator.hasNext()) {
-
 			Employee employee = iterator.next();
 			System.out.println("EMP ID: " + employee.getEmpId() + " EMP NAME :" + employee.getEmpName());
 		}
